cce107cce93cd847088b099c88d8b3c74696d6e5
Daniel Reiter Horn <danielrh@dropbox.com>
2018-04-22 01:48:45 -0700

    adding a smaller test

--- src/probability/common_tests.rs
+++ src/probability/common_tests.rs
@@ -61,3 +61,24 @@ pub fn operation_test_helper<CDFA: CDF16, CDFB: CDF16> (cdf0a: &mut CDFA, cdf1a:
     assert_cdf_similar(&cdf0b.average(cdf1b, 0), cdf1b);
     assert_cdf_similar(&cdf0b.average(cdf1b, all), cdf0b);
 }
+
+mod test {
+use core;
+use std::simd::*;
+#[test]
+fn baseline() {
+   let symbol = 2i16;
+   let inc = 1i16;
+   let data = i16x16::new(4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64);
+   let one_to_16 = i16x16::new(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+   let increment_v = i16x16::splat(inc);
+   let mask_v = unsafe {
+            core::arch::x86_64::_mm256_cmpgt_epi16(core::arch::x86_64::__m256i::from_bits(one_to_16),
+                                                   core::arch::x86_64::__m256i::from_bits(i16x16::splat(i16::from(symbol))))
+    };
+    let output = data + (increment_v & i16x16::from_bits(mask_v));
+    let mut xfinal = [0i16; 16];
+    output.store_unaligned(&mut xfinal);
+    assert_eq!(xfinal, [4, 8, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 65]);
+}
+}
