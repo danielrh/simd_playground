8b5b656700a38e30b53f44c9f2b24c0f9022f3a5
Daniel Reiter Horn <danielrh@dropbox.com>
2018-04-22 01:55:08 -0700

    minimal repro

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
-// Copyright 2017 Dropbox, Inc
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//! This is documentation for the `divans` crate
-//!
-//! The `divans` crate is meant to be used for generic data compression
-
-#![feature(platform_intrinsics)]
 #![feature(stdsimd)]
-extern crate core;
-mod probability;
+mod test {
+#[test]
+fn baseline() {
+use std::simd::*;
+   let symbol = 2i16;
+   let inc = 1i16;
+   let data = i16x16::new(4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64);
+   let one_to_16 = i16x16::new(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+   let increment_v = i16x16::splat(inc);
+   let mask_v = unsafe {
+            ::std::arch::x86_64::_mm256_cmpgt_epi16(::std::arch::x86_64::__m256i::from_bits(one_to_16),
+                                                   ::std::arch::x86_64::__m256i::from_bits(i16x16::splat(i16::from(symbol))))
+    };
+    let output = data + (increment_v & i16x16::from_bits(mask_v));
+    let mut xfinal = [0i16; 16];
+    output.store_unaligned(&mut xfinal);
+    assert_eq!(xfinal, [4, 8, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 65]);
+}
+}
